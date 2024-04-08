f87f24e3db3b394f6f845a6c4aa2391fa11b67ee
Santiago Botero <boterop22@gmail.com>
2024-04-08 00:46:08 -0500

    Add space between location icon and country

--- portfolio/views/header.py
+++ portfolio/views/header.py
@@ -7,17 +7,16 @@ from portfolio.styles.styles import Size
 
 def header(data: Data) -> rx.Component:
     return rx.hstack(
-        rx.avatar(
-            src=data.avatar,
-            size=Size.BIG.value
-        ),
+        rx.avatar(src=data.avatar, size=Size.BIG.value),
         rx.vstack(
             heading(data.name, True),
             heading(data.skill),
-            rx.text(
-                rx.icon("map-pin"),
-                data.location,
-                display="inherit"
+            rx.flex(
+                rx.text(
+                    rx.icon("map-pin"),
+                ),
+                rx.text(data.location, display="inherit"),
+                spacing="1"
             ),
             media(data.media),
             spacing=Size.SMALL.value,
