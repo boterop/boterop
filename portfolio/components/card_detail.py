fb0cbca8e4628eae4c7666ecb72783e648b40373
Santiago Botero <boterop22@gmail.com>
2024-04-08 01:08:33 -0500

    Remove image from extra projects

--- portfolio/components/card_detail.py
+++ portfolio/components/card_detail.py
@@ -7,15 +7,6 @@ from portfolio.styles.styles import IMAGE_HEIGHT, Size
 def card_detail(extra: Extra) -> rx.Component:
     return rx.card(
         rx.link(
-            rx.inset(
-                rx.image(
-                    src=extra.image,
-                    height=IMAGE_HEIGHT,
-                    width="100%",
-                    object_fit="cover"
-                ),
-                pb=Size.DEFAULT.value
-            ),
             rx.text.strong(extra.title),
             rx.text(
                 extra.description,
