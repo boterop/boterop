c45f6afb53fcdc06971b32e519980446b58faf57
Santiago Botero <boterop22@gmail.com>
2024-04-08 01:43:08 -0500

    Add space between title and desc in extra cards

--- portfolio/components/card_detail.py
+++ portfolio/components/card_detail.py
@@ -7,14 +7,14 @@ from portfolio.styles.styles import IMAGE_HEIGHT, Size
 def card_detail(extra: Extra) -> rx.Component:
     return rx.card(
         rx.link(
-            rx.text.strong(extra.title),
-            rx.text(
-                extra.description,
-                size=Size.SMALL.value,
-                color_scheme="gray"
-            )
+            rx.flex(
+                rx.text.strong(extra.title),
+                rx.text(extra.description, size=Size.SMALL.value, color_scheme="gray"),
+                direction="column",
+                spacing="1",
+            ),
+            href=extra.url,
         ),
         width="100%",
-        href=extra.url,
-        is_external=True
+        is_external=True,
     )
