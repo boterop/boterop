047990f9c04096960e28f2559550e63708456613
Santiago Botero <boterop22@gmail.com>
2024-04-20 01:49:24 -0500

    Working on cards

--- portfolio/components/glow_card.py
+++ portfolio/components/glow_card.py
@@ -19,7 +19,21 @@ def glow_card(extra: Extra) -> rx.Component:
         is_external=True,
     )
 
-    return rx.stack(
-        rx.script(src="/scripts/glow_card_script.js"),
-        rx.html("<article data-glow><div data-glow></div></article>"),
+    return rx.card(
+        rx.link(
+            rx.flex(
+                rx.text.strong(extra.title),
+                rx.text(
+                    extra.description,
+                    size=Size.SMALL.value,
+                    color_scheme="gray",
+                ),
+                direction="column",
+                spacing="1",
+            ),
+            href=extra.url,
+        ),
+        width="100%",
+        is_external=True,
+        custom_attrs={"data-glow": ""},
     )
