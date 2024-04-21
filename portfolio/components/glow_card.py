9387d0017239d4fe8d12076ecce954f8ff17853f
Santiago Botero <boterop22@gmail.com>
2024-04-21 01:25:05 -0500

    Update podnation image

--- portfolio/components/glow_card.py
+++ portfolio/components/glow_card.py
@@ -5,20 +5,6 @@ from portfolio.styles.styles import Size
 
 
 def glow_card(extra: Extra) -> rx.Component:
-    card = rx.card(
-        rx.link(
-            rx.flex(
-                rx.text.strong(extra.title),
-                rx.text(extra.description, size=Size.SMALL.value, color_scheme="gray"),
-                direction="column",
-                spacing="1",
-            ),
-            href=extra.url,
-        ),
-        width="100%",
-        is_external=True,
-    )
-
     return rx.box(
         rx.box(
             custom_attrs={"data-glow": ""},
