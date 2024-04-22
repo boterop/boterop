079a29797e6dd73d219e16b97f95dfc081d9370c
boterop <boterop22@gmail.com>
2024-04-22 01:13:25 -0500

    Create copy to clipboard button

--- portfolio/components/icon_button.py
+++ portfolio/components/icon_button.py
@@ -1,13 +1,27 @@
 import reflex as rx
 
 
-def icon_button(icon: str, url: str, text="", solid=False) -> rx.Component:
-    return rx.link(
-        rx.button(
-            rx.icon(icon),
-            text,
-            variant="solid" if solid else "surface"
+def icon_button(
+    icon: str,
+    url: str,
+    text="",
+    solid=False,
+    child="",
+    class_name="",
+) -> rx.Component:
+    background = f"bg-{'cyan-600' if solid else 'transparent'}"
+    return rx.flex(
+        rx.link(
+            rx.flex(
+                rx.icon(icon, color="white"),
+                rx.container(text, class_name="pt-0.5"),
+                spacing="2",
+                class_name="text-white text-sm font-bold",
+            ),
+            href=url,
+            is_external=True,
         ),
-        href=url,
-        is_external=True
+        child,
+        class_name=f"items-center px-3 py-1 {background} {class_name}",
+        spacing="3",
     )
--- portfolio/components/media.py
+++ portfolio/components/media.py
@@ -6,31 +6,27 @@ from portfolio.styles.styles import Size
 
 def media(data: Media) -> rx.Component:
     return rx.flex(
-        icon_button(
-            "mail",
-            f"mailto:{data.email}",
-            data.email,
-            True
+        rx.flex(
+            icon_button(
+                "mail",
+                f"mailto:{data.email}",
+                data.email,
+                True,
+                rx.button(
+                    rx.icon("copy", color="white", size=16),
+                    on_click=rx.set_clipboard(data.email),
+                    variant="ghost",
+                ),
+                "border-inherit border-solid rounded-full",
+            ),
         ),
         rx.hstack(
-            icon_button(
-                "file-text",
-                data.cv
-            ),
-            icon_button(
-                "github",
-                data.github
-            ),
-            icon_button(
-                "linkedin",
-                data.linkedin
-            ),
-            icon_button(
-                "message-circle",
-                data.whatsapp
-            ),
-            spacing=Size.SMALL.value
+            icon_button("file-text", data.cv),
+            icon_button("github", data.github),
+            icon_button("linkedin", data.linkedin),
+            icon_button("message-circle", data.whatsapp),
+            spacing=Size.SMALL.value,
         ),
         spacing=Size.SMALL.value,
-        flex_direction=["column", "column", "row"]
+        flex_direction=["column", "column", "row"],
     )
