31316c8d68fae44b40910ff093a4be6979ad8989
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-04-22 13:46:13 -0500

    Restore icon buttons to avoid errors (#38)

--- portfolio/components/icon_button.py
+++ portfolio/components/icon_button.py
@@ -1,30 +1,9 @@
 import reflex as rx
 
 
-def icon_button(
-    icon: str,
-    url: str,
-    text="",
-    solid=False,
-    child="",
-    class_name="",
-) -> rx.Component:
-    background = "cyan-600" if solid else "transparent"
-    border = "border-0" if solid else "border"
-    border_color = "transparent" if solid else "cyan-700"
-    color = "white" if solid else "cyan-500"
-    return rx.flex(
-        rx.link(
-            rx.flex(
-                rx.icon(icon, class_name=f"stroke-{color}"),
-                rx.container(text, class_name="pt-0.5"),
-                spacing="2" if text != "" else "0",
-                class_name="text-white text-sm font-medium",
-            ),
-            href=url,
-            is_external=True,
-        ),
-        child,
-        class_name=f"items-center h-8 px-3 py-1 rounded-full border-{border_color} bg-{background} {class_name} {border}",
-        spacing="3",
+def icon_button(icon: str, url: str, text="", solid=False) -> rx.Component:
+    return rx.link(
+        rx.button(rx.icon(icon), text, variant="solid" if solid else "surface"),
+        href=url,
+        is_external=True,
     )
