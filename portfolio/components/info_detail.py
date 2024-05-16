0fd0957162325682d1015973def6d39824fe4fae
boterop <boterop22@gmail.com>
2024-05-15 22:37:55 -0500

    Lazy loading and pip modules

--- portfolio/components/info_detail.py
+++ portfolio/components/info_detail.py
@@ -12,11 +12,7 @@ def info_detail(info: Info) -> rx.Component:
             rx.vstack(
                 rx.text.strong(info.title),
                 rx.text(info.subtitle),
-                rx.text(
-                    info.description,
-                    size=Size.SMALL.value,
-                    color_scheme="gray"
-                ),
+                rx.text(info.description, size=Size.SMALL.value, color_scheme="gray"),
                 rx.cond(
                     info.technologies,
                     rx.flex(
@@ -24,63 +20,46 @@ def info_detail(info: Info) -> rx.Component:
                             rx.badge(
                                 rx.box(class_name=technology.icon),
                                 technology.name,
-                                color_scheme="gray"
+                                color_scheme="gray",
                             )
                             for technology in info.technologies
                         ],
                         wrap="wrap",
-                        spacing=Size.SMALL.value
-                    )
+                        spacing=Size.SMALL.value,
+                    ),
                 ),
                 rx.hstack(
-                    rx.cond(
-                        info.url != "",
-                        icon_button(
-                            "link",
-                            info.url
-                        )
-                    ),
-                    rx.cond(
-                        info.github != "",
-                        icon_button(
-                            "github",
-                            info.github
-                        )
-                    )
+                    rx.cond(info.url != "", icon_button("link", info.url)),
+                    rx.cond(info.github != "", icon_button("github", info.github)),
                 ),
                 spacing=Size.SMALL.value,
-                width="100%"
+                width="100%",
             ),
             spacing=Size.DEFAULT.value,
-            width="100%"
+            width="100%",
         ),
         rx.cond(
             info.image != "",
             rx.image(
+                id=f"info-image-{info.title}",
                 src=info.image,
                 height=IMAGE_HEIGHT,
                 width="auto",
                 border_radius=EmSize.DEFAULT.value,
-                object_fit="cover"
-            )
+                object_fit="cover",
+                loading="lazy",
+            ),
         ),
         rx.vstack(
-            rx.cond(
-                info.date != "",
-                rx.badge(info.date)
-            ),
+            rx.cond(info.date != "", rx.badge(info.date)),
             rx.cond(
                 info.certificate != "",
-                icon_button(
-                    "shield-check",
-                    info.certificate,
-                    solid=True
-                )
+                icon_button("shield-check", info.certificate, solid=True),
             ),
             spacing=Size.SMALL.value,
-            align="end"
+            align="end",
         ),
         flex_direction=["column-reverse", "row"],
         spacing=Size.DEFAULT.value,
-        width="100%"
+        width="100%",
     )
