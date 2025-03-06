0525320f6adfeba18485ac89f92eaf1f605ece2c
Santiago Botero <98826652+boterop@users.noreply.github.com>
2025-03-06 11:20:50 -0500

    add reflex icon and builded page (#53)

--- portfolio/views/footer.py
+++ portfolio/views/footer.py
@@ -5,8 +5,23 @@ from portfolio.styles.styles import Size
 
 
 def footer(data: Data) -> rx.Component:
-    return rx.vstack(
-        rx.text(data.name),
-        media(data.media),
-        spacing=Size.SMALL.value
+    made = "Portfolio made with"
+    return rx.hstack(
+        rx.vstack(
+            rx.text(data.name),
+            media(data.media),
+            spacing=Size.SMALL.value,
+        ),
+        rx.center(
+            rx.hstack(
+                rx.text(made),
+                rx.badge(
+                    rx.box(class_name="devicon-reflex-plain", font_size="24px"),
+                    rx.text("Reflex"),
+                    size="2",
+                ),
+            ),
+            width="100%",
+        ),
+        width="100%",
     )
